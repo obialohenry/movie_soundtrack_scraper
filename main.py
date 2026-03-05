194b57a851ec747c20d1ea7bf9e0bbe4c1d91224
sisu <obilaorchisomebi123@gmail.com>
2026-03-05 14:17:06 +0100

    feat: implement Wikipedia Soundtrack search for user-input movies
    - prompts the user to enter a movie name
    - queries Wikepedia API for pages containing the movie title and 'soundtrack'
    - parses the JSON response to extract relevant movie titles
    - uses search_for_a_movie_title to find the first matching title
    - formats the title for URL
    - stops the program if no matching soundtrack page is found

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+
 # ---------- Project Summary ---------- #
 # 1. Prompt the user to enter a movie name
 # 2. Locate and scrape the movie's soundtrack page on Wikipedia.
@@ -17,3 +19,54 @@
 # Example Wikipedia Soundtrack Pages
 # Black Panther: Wakanda Forever: "https://en.wikipedia.org/wiki/Black_Panther%3A_Wakanda_Forever_%28soundtrack%29?utm_source=chatgpt.com"
 # Barbie the Album: "https://en.wikipedia.org/wiki/Barbie_the_Album?utm_source=chatgpt.com"
+
+is_running = True
+
+def search_for_a_movie_title(movies:list[dict],value)-> str:
+  """Search for a movie title containing a given text.
+  
+  Parameters:
+    - movies (list[dict]): A list of movies and their title's
+    - value (str): A substring contained in the movie titles.
+
+  Returns:
+    str: The first movie title that contains the value, or an empty string if no match is found.
+  """
+  value = value.lower()
+  for movie in movies:
+    if value in movie["title"].lower():
+      return movie["title"]
+  return ""
+
+while is_running:
+
+  # ---------- PROMPT THE USER TO ENTER A MOVIE NAME ---------- #
+
+  movie_name = input("Enter a movie name: ").title()
+  WIKIPEDIA_URL = "https://en.wikipedia.org/w/api.php"
+  SOUNDTRACK = "soundtrack"
+  WIKEPEDIA_PARAMS = {
+    "action":"query",
+    "list":"search",
+    "srsearch": f"{movie_name} {SOUNDTRACK}",
+    "utf8":"",
+    "format": "json"
+  }
+  headers = {
+    "User-Agent": "MovieSoundtrackScraper/1.0 (obilaorchisomebi123@gmail.com)"
+  }
+
+  response = requests.get(url=WIKIPEDIA_URL,headers=headers,params=WIKEPEDIA_PARAMS)
+  response.raise_for_status()
+  response_json = response.json()
+  movie_titles = response_json["query"]["search"]
+  movie_title = search_for_a_movie_title(movie_titles,SOUNDTRACK).replace(" ","_")
+  if  movie_title:
+    print(f"Movie Title: {movie_title}")
+    WIKIPEDIA_MOVIE_PAGE_URL = f"https://en.wikipedia.org/wiki/{movie_title}"
+    print(f"Wikipedia Movie Page: { WIKIPEDIA_MOVIE_PAGE_URL}")
+  else:
+    print(f"Could not get {movie_name} Wikipedia's Soundtrack page.")
+    is_running = False
+  
+  # ---------- LOCATE AND SCRAPE THE MOVIES SOUNDTRACK PAGE ON WIKIPEDIA ---------- #
